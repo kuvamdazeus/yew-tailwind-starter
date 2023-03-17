d40404740dfdad4b7e2199266278d333d10ed5ae
Kuvam Bhardwaj <kuvamyash@gmail.com>
2023-03-17 17:06:08 +0530

    - add styles

--- src/app.rs
+++ src/app.rs
@@ -4,7 +4,9 @@ use yew::prelude::*;
 pub fn app() -> Html {
     html! {
         <section class="flex items-center justify-center h-screen w-screen">
-            <p class="text-5xl font-bold">{"👋🏻 Hi from Tailwind!"}</p>
+            <p class="text-5xl font-light py-5 px-10 border border-gray-400 rounded-xl">
+                {"👋🏻 Hi from Rust & Tailwind!"}
+            </p>
         </section>
     }
 }
